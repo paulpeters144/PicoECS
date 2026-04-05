754ef4935658bb8f02e9ded30eff22496a1cf28a
paulpeters144 <paul.peters144@gmail.com>
2026-04-05 05:23:53 -0500

    fix: id generation

--- PicoEntityStore/PicoEntity.cs
+++ PicoEntityStore/PicoEntity.cs
@@ -3,21 +3,16 @@ namespace PicoEntityStoreCore;
 /// <summary>
 /// Mandatory base class for all entities in the PicoEntityStore store.
 /// </summary>
+using System.Security.Cryptography;
+
 public abstract class PicoEntity
 {
-    private static uint _nextId = 0;
-
-    /// <summary>
-    /// Unique identifier for the entity.
-    /// </summary>
     public uint Id { get; }
-
     internal uint ParentId { get; set; }
     internal uint[] ChildIds { get; set; } = [];
     internal int TypeListIndex { get; set; } = -1;
-
     protected PicoEntity()
     {
-        Id = Interlocked.Increment(ref _nextId);
+        Id = BitConverter.ToUInt32(RandomNumberGenerator.GetBytes(4));
     }
 }
